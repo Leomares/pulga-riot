11baa9e4116dc33f9dcf3a17e49d0d75f317cb76
Leomares <leonardopires25@gmail.com>
2023-08-30 15:07:05 -0300

    data from sensor is stored in a db at the end of execution

--- examples/nimble_gatt/datalogger/datalogger.py
+++ examples/nimble_gatt/datalogger/datalogger.py
@@ -1,9 +1,37 @@
-#choose the right port
-#works on windows
+# choose the right port
+# works on windows
 
 import serial
 import time
 import matplotlib.pyplot as plt
+import sqlite3
+import atexit
+
+
+def exitHandler():
+    connection = sqlite3.connect('heater.db')
+    cursor = connection.cursor()
+    cursor.execute('')
+
+    create_table_query = '''
+        CREATE TABLE sensordata (
+            id INTEGER PRIMARY KEY,
+            x REAL,
+            y REAL,
+            z REAL,
+            t REAL
+        )
+    '''
+    cursor.execute(create_table_query)
+    connection.commit()
+    insert_query = 'INSERT INTO sensordata VALUES(?,?,?,?)'
+    for i in range(len(x)):
+        cursor.execute(insert_query, (x[i], y[i], z[i], t[i]))
+    connection.commit()
+    connection.close()
+
+
+atexit.register(exitHandler)
 
 x, y, z, t = [], [], [], []
 length = 0
@@ -14,7 +42,7 @@ pot = [0 for i in range(3)]
 
 fig, axs = plt.subplots(4, 1, figsize=(12, 6))
 legends = ["x", "y", "z", "t"]
-ylabel = [a + '"(g)' for a in legends] + ["temp(C)"]
+# ylabel = [a + '"(g)' for a in legends] + ["temp(C)"]
 
 port = serial.Serial("COM3", 115200)
 
@@ -44,17 +72,17 @@ while 1:
             count = 0
             for ax in axs:
                 ax.clear()
-            axs[0].set_title("Pulga")
+            axs[0].set_title("BMI/BME - Pulga")
             axs[-1].set_xlabel("Timestamp")
-            axs[0].plot(x, label="x", color="red")
-            axs[1].plot(y, label="y", color="blue")
-            axs[2].plot(z, label="z", color="green")
-            axs[3].plot(t, label="t", color="pink")
+            axs[0].plot(x, label="x(g)", color="red")
+            axs[1].plot(y, label="y(g)", color="blue")
+            axs[2].plot(z, label="z(g)", color="green")
+            axs[3].plot(t, label="t(C)", color="pink")
             for ax in axs[:-1]:
                 right = max(length, window)
                 ax.set_xlim(right - window, right)
                 ax.legend(loc="upper right")
             plt.pause(0.1)
             plt.show(block=False)
-            plt.savefig(f"data{length/100}.png")
+            # plt.savefig("imgs/"+f"data{length/100}.png")
             # time.sleep(0.1)
